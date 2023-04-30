c0818bd9d90049e6abe9fb6f76fe850431582e69
leader-akash <leader.akashak@gmail.com>
2023-04-30 22:04:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+var outputdiv= document.querySelector("#output");
+
+// outputdiv.innerText ="Akash" for  writing already
+
+console.log(outputdiv);
+
+function clickHandler(){
+    outputdiv,innerText = "hgvhhh" + textInput.value;
+}
+
+btnTranslate.addEventListener("click", clickHandler)
+
