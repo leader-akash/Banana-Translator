09b8f737dc2e176b4dce160f6044dd9bbefc4543
leader-akash <leader.akashak@gmail.com>
2023-05-01 13:52:15 +0530

    finished app

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-
 var btnTranslate = document.querySelector("#btn-translate");
-var textInput = document.querySelector("#text-input");
-var outputdiv= document.querySelector("#output");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-// outputdiv.innerText ="Akash" for  writing already
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-console.log(outputdiv);
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler(){
-    outputdiv,innerText = "hgvhhh" + textInput.value;
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 }
 
-btnTranslate.addEventListener("click", clickHandler)
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
